8d96a75d4aad045807d6c5881ad52e96a692ebfe
samvr-pxl <sam.vanrijn@pxl.be>
2022-10-04 20:20:08 +0200

    added better flask support + removed vim stuff

--- app/app.py
+++ app/app.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+import pickle
+from prophet import Prophet
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!!!"
+with open('forecast_model.pckl', 'rb') as fin:
+    model = pickle.load(fin)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/api/v1.0/forecast/energy', methods=['POST'])
+def predict():
+    days = int(request.json['days'])
+    future = model.make_future_dataframe(periods=days)
+    forecast = model.predict(future)
+    data = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']][-days:]
+
+    result = data.to_json(orient='records', date_format='iso')
+
+    return result
 
 if __name__ == '__main__':
-    host = "0.0.0.0"
-    port = 5000
-    debug = False
+    app.run(host='0.0.0.0')
 
-    app.run(host, port, debug)
